cc133e92fed41440b89b5baf5dee0a2556ae74db
Bharath9t7 <bharath9t7@gmail.com>
2023-05-17 16:34:49 +0530

    feature test

--- plugins/var.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-airflow variables set s3_bucket_data "$S3_BUCKET_DATA"
-airflow variables set var_test "$test_var"
--- scripts/README.md
+++
@@ -1,1 +0,0 @@
-this folder is being used to test s3 sync command functionality
